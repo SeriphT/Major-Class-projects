44e7ed5203b16da04e7d181d005d41422e757d1b
SeriphT <42582157+SeriphT@users.noreply.github.com>
2018-08-31 10:03:59 -0600

    Add files via upload

---
+++ Pygame template.py
@@ -0,0 +1,43 @@
+#Sabastian Taton
+#Aug. 31
+#Pygame Skeleton
+
+
+#imports
+import  pygame
+import random
+
+
+#window
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
+
+#defining colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED
+GREEN
+BLUE
+
+
+#Pygame initialization and window setup
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH,  HEIGHT))
+pygame.display.set_caption("Game  Name")
+clock = pygame.time.Clock()
+
+
+#Game loop
+running = True
+while  running:
+        #Process input
+        
+
+        #Update
+
+
+        #Render
+        screen.fill(())
+            
