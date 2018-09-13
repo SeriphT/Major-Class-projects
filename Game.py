2d0cf7b23e7562e513157d49caaa3f3dc51966e2
SeriphT <42582157+SeriphT@users.noreply.github.com>
2018-09-13 10:12:47 -0600

    Shmup Project

--- Game.py
+++ Game.py
@@ -26,37 +26,72 @@ img_folder = os.path.join(game_folder, "img")
 
 
 #Sprites
+        #Player Sprite
 class Player(pygame.sprite.Sprite):
- def __init__(self):
-     pygame.sprite.Sprite.__init__(self)
-     ## self.image = pygame.image.load(os.path.join(img_folder, "ship.png")).convert
-    ##self.image.set_colorkey(BLACK)
-    ##  self.image.fill(BLACK)
-    ##  self.rect = self.image.get_rect()
-    ##  self.rect.center = (WIDTH/2, HEIGHT/2)
-    ##  self.y_speed = 5
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join(img_folder, "skull.png")).convert()
+        self.image.set_colorkey(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH/2, HEIGHT/2)
 
-#rectangle for testing purposes
-     self.image = pygame.Surface((50,40))
-     self.image.fill(GREEN)
-     self.rect = self.image.get_rect()
-    self.rect.centerx = WIDTH/2
-    self.rect.bottom = HEIGHT - 10
-    self.speedx = 0
-    
-def update(self):
-        self.speedx = 0
-        keystate = pygame.key.get_pressed()
-        if  keystate[pygame.K_LEFT] :
-               self.speedx = -5
-        if  keystate[pygame.K_RIGHT] :
+    def update(self):
+            self.speedx = 0
+            keystate = pygame.key.get_pressed()
+            if  keystate[pygame.K_a]:
+                self.speedx = -5
+            if  keystate[pygame.K_d]:
                 self.speedx = 5
-         self.rect.x = self.speedx
-        if self.rect.right > WIDTH :
-            self.rect.right = WIDTH
-        if self.rect.left  < 0:
-            self.rect.left = 0
-         
+            self.rect.x += self.speedx
+            if self.rect.right > WIDTH:
+                self.rect.right = WIDTH
+            if self.rect.left < 0:
+                self.rect.left = 0
+
+## Testing Rectangle
+##             pygame.sprite.Sprite.__init__(self)
+##             self.image = pygame.Surface((50,40))
+##             self.image.fill(WHITE)
+##             self.rect = self.image.get_rect()
+##             self.rect.centerx = WIDTH/2
+##             self.rect.centery = HEIGHT - 15
+##             self.speedx = 0
+
+    def update(self):
+            self.speedx = 0
+            keystate = pygame.key.get_pressed()
+            if  keystate[pygame.K_a]:
+                self.speedx = -5
+            if  keystate[pygame.K_d]:
+                self.speedx = 5
+            self.rect.x += self.speedx
+            if self.rect.right > WIDTH:
+                self.rect.right = WIDTH
+            if self.rect.left < 0:
+                self.rect.left = 0
+
+        ##Mob Sprites
+
+        #Low Enemy
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30,40))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange( -100, -40)
+        self.speedy = random.randrange(1,8)
+        self.speedx = 1
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT +10:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange( -100, -40)
+            self.speedy = random.randrange(1,8)
+        
+
 #Pygame initialization and window setup
 pygame.init()
 pygame.mixer.init()
@@ -67,6 +102,12 @@ clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
+mobs = pygame.sprite.Group()
+for i in range(12):
+    m = Mob()
+    all_sprites.add(m)
+    mobs.add(m)
+    
 
 #Game loop
 running = True
@@ -74,18 +115,18 @@ while  running:
     #keeps game running at right spped
     clock.tick(FPS)
         #Process input (events)
-        for event in pygame.event.get():
+    for event in pygame.event.get():
                 #check for window close
                 if  event.type == pygame.QUIT:
                         running = False
                 
         #Update
-        all_sprites.update()
+    all_sprites.update()
         
         #Render
-        screen.fill(BLUE)
-        all_sprites.draw(screen)
+    screen.fill(BLUE)
+    all_sprites.draw(screen)
          #after drawing everything flip display
-        pygame.display.flip()
-        
-        pygame.quit()
+    pygame.display.flip()
+
+pygame.quit()
