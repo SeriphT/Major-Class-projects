8e494082c6dff8a9327cf08cd1c5a8b95b5ac06a
SeriphT <42582157+SeriphT@users.noreply.github.com>
2019-04-16 10:54:20 -0600

    Update shmup.py

--- SHMUP/shmup.py
+++ SHMUP/shmup.py
@@ -266,15 +266,39 @@ for i in range(8):
 score = 0
 pygame.mixer.music.play(loops=-1)
 # Game loop
+game_over = True
 running = True
 while running:
-    # keep loop running at the right speed
-    clock.tick(FPS)
-    # Process input (events)
-    for event in pygame.event.get():
-        # check for closing window
-        if event.type == pygame.QUIT:
-            running = False
+    if game_over = True:
+        show_go_screen()
+        game_over = False
+        all_sprites = pygame.sprite.Group()
+        mobs = pygame.sprite.Group()
+        bullets = pygame.sprite.Group()
+        powerups = pygame.sprite.Group()
+        player = Player()
+        all_sprites.add(player)
+        for i in range(8):
+            newmob()
+        score = 0
+        if player.lives == 0 and not death_explosion.alive():
+            game_over = True
+            
+def show_go_screen():
+    screen.blit(background, background_rect)
+    draw_text(screen, "SHMUP!", 64, WIDTH / 2, HEIGHT / 4)
+    draw_text(screen, "Arrow keys move, Space to fire", 22,
+              WIDTH / 2, HEIGHT / 2)
+    draw_text(screen, "Press a key to begin", 18, WIDTH / 2, HEIGHT * 3 / 4)
+    pygame.display.flip()
+    waiting = True
+    while waiting:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYUP:
+                waiting = False
 
     # Update
     all_sprites.update()
